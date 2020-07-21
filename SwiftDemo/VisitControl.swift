86349a9e0fffac5491fe84df8ab876ae59f94446
liuqi <754854967@qq.com>
2020-07-21 23:22:39 +0800

    add

---
+++ SwiftDemo/VisitControl.swift
@@ -0,0 +1,18 @@
+//
+//  VisitControl.swift
+//  SwiftDemo
+//
+//  Created by Qi Liu on 2020/7/15.
+//  Copyright © 2020 Qi Liu. All rights reserved.
+//
+
+import Foundation
+public class SomePublicClass {}
+internal class SomeInternalClass {}
+fileprivate class SomeFilePrivateClass {}
+private class SomePrivateClass {}
+ 
+public var somePublicVariable = 0
+internal let someInternalConstant = 0
+fileprivate func someFilePrivateFunction() {}
+private func somePrivateFunction() {}
